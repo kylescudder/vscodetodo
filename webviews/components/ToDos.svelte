e9b3379b53bbb276b793573404e16fb47b6a4389
Kyle Scudder <kyle.s@paloma.co.uk>
2022-03-28 08:49:38 +0100

    li changed to article

--- webviews/components/ToDos.svelte
+++ webviews/components/ToDos.svelte
@@ -172,7 +172,7 @@
     <ul class="collapsed">
       {#each todos as todo (todo.id)}
         {#if categories.id === todo.categorieId}
-          <li
+          <article class="card"
             class:completed={todo.completed}
             on:click={async () => {
               todo.completed = !todo.completed;
@@ -202,7 +202,6 @@
             }}
           >
             {todo.text}
-          </li>
             {#if todo.completed}
                 <svg xmlns="http://www.w3.org/2000/svg" height="40" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
@@ -213,6 +212,7 @@
                   <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
                 </svg>
             {/if}
+              </article>
         {/if}
       {/each}
     </ul>
