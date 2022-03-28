8546538f7581b2eb59d896145cc1fa90c4055d9a
Kyle Scudder <kyle.s@paloma.co.uk>
2022-03-28 08:48:25 +0100

    Added rollup-plugin-css-only to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
+import css from "rollup-plugin-css-only";
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import path from "path";
