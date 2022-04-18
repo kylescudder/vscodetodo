9c71b16b7506101f9327133dc04f41e148427fd8
kylescudder <74150974+kylescudder@users.noreply.github.com>
2022-04-18 17:38:30 +0100

    Added tailwindcss to project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  plugins: [],
+  content: ["./webviews/**/*.svelte"],
+  important: true,
+};
