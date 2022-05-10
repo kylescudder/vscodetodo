e152e177b6d0fd1fb9a4a88e1075c14464bafd87
kylescudder <74150974+kylescudder@users.noreply.github.com>
2022-05-10 15:57:27 +0100

    Updated API url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const apiBaseUrl = `https://resonant-acute-ranunculus.glitch.me/VSCodeToDo`;
+export const apiBaseUrl = `https://kylescudder-api.herokuapp.com/VSCodeToDo`;
 //export const apiBaseUrl = `http://localhost:5556/VSCodeToDo`;
 export const extensionAPIPort = `54322`;
